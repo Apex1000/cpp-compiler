3b6ae19b59d04d5178fd5f55777d5153948cd295
Siddhartha Vikram <siddhartsingh9876@gmail.com>
2017-01-26 17:14:17 +0530

    only work in linux

--- cpp.php
+++ cpp.php
@@ -11,8 +11,19 @@ if(!empty($_POST))
 	$ans=shell_exec("./hello");
 }
 ?>
-<form method="POST">
-	<textarea name="cppcode" class="txtarea"><?php echo $current; ?></textarea>
-	<input type="submit" value="Run">
-	<textarea name="cppcode2" disabled class="txtarea"><?php echo $ans; ?></textarea>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Compiler</title>
+</head>
+<body>
+<center><h1>C/C++ Compiler </h1></center>
+<center><form method="POST">
+	<textarea name="cppcode" class="txtarea" rows="30" cols="150"><?php echo $current; ?></textarea><br><br>
+	<input type="submit" value="Run"><br><br>
+	<textarea name="cppcode2" disabled class="txtarea" rows="10" cols="100"><?php echo $ans; ?></textarea>
 </form>
+</center>
+</body>
+</html>
+
